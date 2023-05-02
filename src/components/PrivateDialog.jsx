19df6933c11076f595beb42d73ed1c7c48a26c02
leostelon <53221136+leostelon@users.noreply.github.com>
2023-05-02 14:29:41 +0530

    private wo/ crypto

---
+++ src/components/PrivateDialog.jsx
@@ -0,0 +1,68 @@
+import { Box, CircularProgress, Dialog } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { updatePrivateRepo } from "../api/repository";
+
+export const PrivateDialog = ({ image, isOpen, handleExternalClose }) => {
+	const [loading, setLoading] = useState(false);
+	const [open, setOpen] = useState(false);
+
+	const handleClose = () => {
+		setOpen(false);
+		if (handleExternalClose) {
+			handleExternalClose();
+		}
+	};
+
+	async function makePrivate(image) {
+		setLoading(true);
+		await updatePrivateRepo(image);
+		setLoading(true);
+		window.location.reload();
+	}
+
+	useEffect(() => {
+		if (isOpen) {
+			setOpen(true);
+		}
+	}, [isOpen]);
+
+	return (
+		<Dialog open={open} onClose={handleClose} fullWidth maxWidth="xs">
+			<Box
+				sx={{
+					backgroundColor: "#e9e9e9",
+					padding: "12px",
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+				}}
+			>
+				<h2>Private Repository</h2>
+				<Box sx={{ mt: 2, mb: 2, fontSize: "16px", fontWeight: "600" }}>
+					Do you wish to make it {image.private ? "public" : "private"}?
+				</Box>
+				<Box
+					sx={{
+						cursor: "pointer",
+						padding: "8px 12px",
+						borderRadius: "4px",
+						backgroundColor: "#ff92a2",
+						color: "white",
+						width: "fit-content",
+						fontWeight: "600",
+						minWidth: "100px",
+						display: "flex",
+						justifyContent: "center",
+					}}
+					onClick={() => makePrivate(image)}
+				>
+					{loading ? (
+						<CircularProgress size={14} sx={{ color: "white" }} />
+					) : (
+						`Make ${image.private ? "Public" : "Private"}`
+					)}
+				</Box>
+			</Box>
+		</Dialog>
+	);
+};
